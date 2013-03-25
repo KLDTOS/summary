1e9163ae631627d27f092183839d9880b14bd9fd
KLDTKS <jksdgb@163.com>
2013-03-25 14:17:01 +0800

    更改仓库结构

--- gen/code_segment/summary/pinganprojectsummary/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package code_segment.summary.pinganprojectsummary;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
